51e371dec2e542b7388fb29d15aae60cd78ec090
Gabriele Amorosino <43575424+gamorosino@users.noreply.github.com>
2026-03-17 10:27:02 -0500

    Add data_is_shelled option to EDDY_OPTS

--- topup_eddy.sh
+++ topup_eddy.sh
@@ -269,6 +269,7 @@ merge_full=$(jq -r '.mergefull // "false"' "$CFG")
 sstrip=$(jq -r '.sstrip // "false"' "$CFG")
 eddy_cuda=$(jq -r '.eddy_cuda // "false"' "$CFG")
 cuda_version=$(jq -r '.cuda_version // empty' "$CFG")
+data_is_shelled=$(jq -r '.data_is_shelled // "false"' "$CFG")
 
 DEBUG=1
 
@@ -859,8 +860,11 @@ else
 
   # NOTE: removed --ref_scan because many builds don't support it; add back only if your eddy supports it.
 
+	
 	EDDY_OPTS=()
-	EDDY_OPTS+=(--data_is_shelled)   # comment this out if you don't want it always
+	if [[ "$data_is_shelled" == "true" ]]; then
+	  EDDY_OPTS+=(--data_is_shelled)
+	fi
 
 	[[ "${DEBUG:-0}" -eq 1 ]] && debug_dump
 	sanitize_and_validate_eddy_inputs
