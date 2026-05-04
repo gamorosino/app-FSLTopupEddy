8cd99edf39d4dcf0daeefb42f9f861a94255008c
Gabriele Amorosino <43575424+gamorosino@users.noreply.github.com>
2026-05-04 10:00:16 -0500

    Update createRegressorsTSV.py

--- createRegressorsTSV.py
+++ createRegressorsTSV.py
@@ -4,28 +4,27 @@ import os, sys
 import pandas as pd
 import json
 
-def createRegressors(param_filepath,movement_filepath,restricted_movement_filepath,outpath):
+def createRegressors(param_filepath, movement_filepath, restricted_movement_filepath, outpath):
 
-    # load data
-    params = pd.read_table(param_filepath, delimiter="  ", header=None)
-    rms = pd.read_table(movement_filepath, delimiter="  ", header=None)
-    rms_restricted = pd.read_table(restricted_movement_filepath, delimiter="  ", header=None)
+    params = pd.read_csv(param_filepath, sep=r"\s+", header=None)
+    rms = pd.read_csv(movement_filepath, sep=r"\s+", header=None)
+    rms_restricted = pd.read_csv(restricted_movement_filepath, sep=r"\s+", header=None)
 
-    # update column names
-    param_columns = ['trans_x','trans_y','trans_z','rot_x','rot_y','rot_z']
-    param_columns = param_columns + [ 'EC'+str(f+1) for f in range(0,len(params.columns) - len(param_columns)) ]
+    param_columns = ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z']
+    param_columns += ['EC' + str(f + 1) for f in range(len(params.columns) - len(param_columns))]
     params.columns = param_columns
 
-    rms_columns = ['framewise_displacement_first_volume','framewise_displacement_previous_volume']
-    rms.columns = rms_columns
+    rms.columns = [
+        'framewise_displacement_first_volume',
+        'framewise_displacement_previous_volume'
+    ]
 
-    rms_restricted_columns = ['framewise_displacement_restricted_first_volume','framewise_displacement_restricted_previous_volume']
-    rms_restricted.columns = rms_restricted_columns
+    rms_restricted.columns = [
+        'framewise_displacement_restricted_first_volume',
+        'framewise_displacement_restricted_previous_volume'
+    ]
 
-    # merge tables together
-    out_df = pd.concat([params,rms,rms_restricted],axis=1)
-
-    # output table
+    out_df = pd.concat([params, rms, rms_restricted], axis=1)
     out_df.to_csv(outpath, sep="\t", index=False)
 
 def main():
